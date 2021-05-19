23ac3129a6467eadb1086211929eb50628d77a07
Matthew Fisher <mwfisher91@gmail.com>
2021-05-19 15:00:48 -0700

    Added _getValues(), filter(), and reduce() methods

--- ProtoArray.js
+++ ProtoArray.js
@@ -11,23 +11,35 @@ class ProtoArray {
     return values;
   }
   _display(){
-    console.log(Object.values(this.values));
+    console.log(this._getValues());
   }
-  forEach(callback){
-    const array = Object.values(this.values);
+  _getValues(){
+    return Object.values(this.values);
+  }
+  filter(callbackFn){
+    let filteredArray = new ProtoArray();
+    const array = this._getValues();
+    for(let index = 0; index < this.length; index++){
+      // MDN: filter((element, index, array) => { ... } )
+      callbackFn(this.values[index], index, array) && filteredArray.push(this.values[index]);
+    }
+    return filteredArray;
+  }
+  forEach(callbackFn){
+    const array = this._getValues();
     for (let index = 0; index < this.length; index++){
       // MDN: forEach((element, index, array) => { ... } )
-      callback(this.values[index], index, array);
+      callbackFn(this.values[index], index, array);
     }
   }
-  map(callback){
-    let newArray = new ProtoArray();
-    const array = Object.values(this.values);
+  map(callbackFn){
+    let mappedArray = new ProtoArray();
+    const array = this._getValues();
     for(let index = 0; index < this.length; index++){
       // MDN: map((element, index, array) => { ... } )
-      newArray.push(callback(this.values[index], index, array));
+      mappedArray.push(callbackFn(this.values[index], index, array));
     }
-    return newArray;
+    return mappedArray;
   }
   push(val){
     this.values[this.length] = val;
@@ -38,5 +50,14 @@ class ProtoArray {
     const val = this.values[this.length];
     delete this.values[this.length];
     return val;
+  }
+  reduce(callbackFn, initValue){
+    let accumulated = initValue;
+    const array = this._getValues();
+    for(let index = 0; index < this.length; index++){
+      // MDN: reduce((accumulator, currentValue, index, array) => { ... } )
+      accumulated = callbackFn(accumulated, this.values[index], index, array);
+    }
+    return accumulated;
   }
 }
