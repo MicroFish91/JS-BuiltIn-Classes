820b5dae074b97bd44272e8d82bc0a8fb13ecbfc
MicroFish91 <mwfisher91@gmail.com>
2021-08-29 14:00:09 -0500

    Updated testing #reverse()

--- test/ProtoArray/reverse.test.js
+++ test/ProtoArray/reverse.test.js
@@ -4,11 +4,14 @@ const ProtoArray = require("../../ProtoArray/ProtoArray.js");
 module.exports = function () {
   describe("#reverse()", function () {
     it("reverse() method reverses an array in place", function () {
-      const myArray = new ProtoArray("one", "two", "three");
-      const result = myArray.reverse();
+      const myArrayOdd = new ProtoArray("one", "two", "three");
+      const myArrayEven = new ProtoArray("one", "two", "three", "four");
+      const result = myArrayOdd.reverse();
+      const resultTwo = myArrayEven.reverse();
 
-      expect(myArray).to.eql(new ProtoArray("three", "two", "one"));
+      expect(myArrayOdd).to.eql(new ProtoArray("three", "two", "one"));
       expect(result).to.eql(new ProtoArray("three", "two", "one"));
+      expect(resultTwo).to.eql(new ProtoArray("four", "three", "two", "one"));
     });
   }); // End describe
 }; // End wrapper function
